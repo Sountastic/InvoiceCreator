64c8b3bb3c496c64dd00d76bf7c0226f691598f4
sountastic <paulina.gr@o2.pl>
2020-06-08 20:03:45 +0200

    Revert "Change TableView for Invoice in ShowData"
    This reverts commit 47eaaf7a9e38108d8dd8701719807b2bca571e6f.

--- src/main/java/javafx/Main.java
+++ src/main/java/javafx/Main.java
@@ -2,13 +2,10 @@ package javafx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.invoicesys.InvoicePdf;
 import javafx.invoicesys.entity.Customer;
-import javafx.invoicesys.entity.Invoice;
 import javafx.invoicesys.entity.Product;
 import javafx.invoicesys.entity.User;
 import javafx.invoicesys.repository.CustomersRepository;
-import javafx.invoicesys.repository.InvoiceRepository;
 import javafx.invoicesys.repository.ProductRepository;
 import javafx.invoicesys.repository.UserRepository;
 import javafx.scene.Parent;
@@ -19,18 +16,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-
-import static javafx.invoicesys.entity.Invoice.builder;
 
 @SpringBootApplication
 public class Main extends Application {
     private ConfigurableApplicationContext springContext;
 
     public static void main(String[] args) {
+//        launch(args);
+//        InvoicePdf ip = new InvoicePdf();
+//        ip.createPdf();
         launch(args);
     }
 
@@ -41,14 +37,13 @@ public class Main extends Application {
         CustomersRepository customersRepository = springContext.getBean(CustomersRepository.class);
         UserRepository userRepository = springContext.getBean(UserRepository.class);
         ProductRepository productRepository = springContext.getBean(ProductRepository.class);
-        InvoiceRepository invoiceRepository = springContext.getBean(InvoiceRepository.class);
 
         Customer customer = Customer.builder()
                 .customerFirstName("Shrek")
                 .customerLastName("Wonderful")
                 .customerAddress("Here 12")
                 .customerCity("Krakow")
-                .customerCompanyName("fhk")
+                .customerCompanyName("Fuck off")
                 .customerEmail("foo@bar.com")
                 .customerNip("111")
                 .build();
@@ -71,20 +66,6 @@ public class Main extends Application {
         products.add(Product.builder().description("drugs").price(12.0).build());
         products.add(Product.builder().description("pizza").price(3.0).build());
         productRepository.saveAll(products);
-
-        Invoice invoice = builder()
-                .id(7L)
-                .date(LocalDate.now())
-                .dueDate(LocalDate.of(2020, 06, 25))
-                .user(user)
-                .customer(customer)
-                .total(350.00)
-                .build();
-        invoiceRepository.save(invoice);
-//
-//        InvoicePdf ip = new InvoicePdf();
-//        ip.getDataFromRepositoryById(7L);
-//        ip.createPdf();
     }
 
     @Override
@@ -96,6 +77,5 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root));
 //        Scene scene = new Scene(root);
         primaryStage.show();
-
     }
 }
--- src/main/java/javafx/invoicesys/repository/InvoiceProductsRepository.java
+++
@@ -1,7 +0,0 @@
-package javafx.invoicesys.repository;
-
-import javafx.invoicesys.entity.InvoiceProduct;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InvoiceProductsRepository extends JpaRepository<InvoiceProduct, Long> {
-}
