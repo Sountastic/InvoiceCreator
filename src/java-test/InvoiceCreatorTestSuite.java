9b8dd0e1471b6677dbb6c25bc11041273d7e15ee
sountastic <paulina.gr@o2.pl>
2020-06-16 20:02:16 +0200

    clear tests

--- src/java-test/InvoiceCreatorTestSuite.java
+++ src/java-test/InvoiceCreatorTestSuite.java
@@ -17,6 +17,7 @@ import java.time.LocalDate;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class InvoiceCreatorTestSuite {
+
     @Autowired
     InvoiceRepository invoiceRepository;
     @Autowired
@@ -27,37 +28,6 @@ public class InvoiceCreatorTestSuite {
 
     @Test
     public void testPdfCreateMethod() {
-        Customer customer = Customer.builder()
-                .customerFirstName("Rod")
-                .customerLastName("Wonderful")
-                .customerAddress("Here 12")
-                .customerCity("Krakow")
-                .customerCompanyName("Foo")
-                .customerEmail("foo@bar.com")
-                .customerNip("123")
-                .build();
-
-        User user = User.builder()
-                .userAddress("lalal")
-                .userCity("Katowice")
-                .userCompanyName("Kakak")
-                .userEmail("nopw@nopw.pl")
-                .userFirstName("John")
-                .userLastName("Snow")
-                .userNip("345")
-                .build();
-
-        Invoice invoice = Invoice.builder()
-                .id(7L)
-                .date(LocalDate.now())
-                .dueDate(LocalDate.of(2020, 06, 25))
-                .user(user)
-                .customer(customer)
-                .total(350.00)
-                .build();
-
-        InvoicePdf invoicePdf = new InvoicePdf(invoiceRepository);
-        invoicePdf.createPdf(invoice);
 
     }
 
